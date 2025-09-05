f990f41bc96f10e046e6aeecc6e6e46f4daf9d5f
Richmond Addai <richmondaddai46@gmail.com>
2025-09-05 13:59:10 +0000

    final build bug fixes

--- frontend/cognita-frontend/src/App.jsx
+++ frontend/cognita-frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { ArrowUpIcon, SparklesIcon } from '@heroicons/react/24/outline';
 import './App.css';
 import './index.css';
 import sendButtonIcon from "@/assets/send_button.svg"; 
-import Logo from './assets/logo_2.png';
+import Logo from '@/assets/logo_2.png';
 
 // ... rest of your code stays the same
 
