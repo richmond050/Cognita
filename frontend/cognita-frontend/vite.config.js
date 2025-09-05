9169bd0354ab73c16a2b03a7f05656144b2c495e
Richmond Addai <richmondaddai46@gmail.com>
2025-09-05 14:01:17 +0000

    Fixes

--- frontend/cognita-frontend/vite.config.js
+++ frontend/cognita-frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   assetsInclude: ['**/*.svg'],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
